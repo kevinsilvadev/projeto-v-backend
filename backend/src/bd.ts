19d5f3d7a6937566e10155cdcc4135e9f424002d
kevinsilvadev <kivim2018@gmail.com>
2023-05-22 16:58:13 -0300

    Alterando o arquivo bd.ts

--- backend/src/bd.ts
+++ backend/src/bd.ts
@@ -3,25 +3,13 @@ var Connection = require('tedious').Connection;
 
 dotenv.config()
 
-/*export const sqlConfig = {
-    server: "ZULU18" + "\\" + "SQLEXPRESS",
-    database: process.env.DATABASE_NAME,
-    user: process.env.USER,
-    password:process.env.PASSWORD,
-    options: {
-        encrypt:true,
-        trustServerCertificate:true
-    }
-}*/
-
-
 export const config = {  
     server: 'dpaschoal-teste.database.windows.net', 
     authentication: {
         type: 'default',
         options: {
-            userName: 'teste',
-            password: '123@qwer'  
+            userName: process.env.USER,
+            password: process.env.PASSWORD  
         }
     },
     options: {
