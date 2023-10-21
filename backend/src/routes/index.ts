ce38395d11a0384e1bf251b1ee8b6c42666f77cf
kevinsilvadev <kivim2018@gmail.com>
2023-10-21 18:59:39 -0300

    Mapeando o banco de dados com o prisma e express 2

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,32 +1,5 @@
-/*import { Request, Response, Router } from 'express';
-import sql from 'mssql';
-export const router: Router = Router();
-
-
-import usuarioController from '../controllers/UsuariosController';
-const usuario: usuarioController = new usuarioController();
-
-
-//Rota dos usurario
-router.get('/usuario', usuario.listarUsuario)
-router.get('/usuario/:id', usuario.findById)
-router.post('/usuario', usuario.criarUsuario)
-
-
-  
-
-//Rota de login
-router.post('/login', usuario.loginUsuario)
-
-
-//Rota dos cursos
-router.get('/cursos', cursosController.listarCursos)
-router.post('/cursos', cursosController.criarCursos)
-*/
-
 import { Request, Response, Router } from 'express';
 export const router: Router = Router();
-
 
 import CursosController from '../controllers/CursosController'
 const cursosController: CursosController = new CursosController();
